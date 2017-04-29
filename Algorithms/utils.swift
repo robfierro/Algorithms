93a7f46f49678329bc3526b14f40773684bfe1a3
Roberto Fierro Martinez <roberto.fierro.mtz@gmail.com>
2017-04-29 03:13:13 -0400

    chg: added a generic Stack data structure in utils

--- Algorithms/utils.swift
+++ Algorithms/utils.swift
@@ -9,6 +9,41 @@
 import Foundation
 import UIKit
 
+// MARK: - Generics
+
+struct Stack<Element> {
+    fileprivate var array: [Element] = []
+    
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    var count: Int {
+        return array.count
+    }
+    
+    var description: String {
+        
+        let topDivider = "---Stack---\n"
+        let bottomDivider = "\n-----------\n"
+        let stackElements = array.map { "\($0)" }.reversed().joined(separator: "\n")
+
+        return topDivider + stackElements + bottomDivider
+    }
+    
+    mutating func push(_ element: Element) {
+        array.append(element)
+    }
+    
+    mutating func pop() -> Element? {
+        return array.popLast()
+    }
+    
+    func peek() -> Element? {
+        return array.last
+    }
+}
+
 // MARK: - Foundation Utils
 
 extension Int {
